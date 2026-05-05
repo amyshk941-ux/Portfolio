29fb3407976ca7f6cd9cefeea8291a481f28f98e
Ahmad <amyshk941@gmail.com>
2026-05-05 10:30:37 +0500

    testnow

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -22,9 +22,9 @@ sudo rsync -av --delete "$BUILD_DIR/" "$TARGET_DIR/"
 echo "===== Fix Permissions ====="
 sudo chown -R www-data:www-data "$TARGET_DIR"
 
+systemctl daemon-reload
 echo "===== Restarting Nginx ====="
 sudo systemctl restart nginx
 
 echo "===== Done ====="
 
-systemctl daemon-reload
